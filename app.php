c247e9e74a7800cf181825fe72c4393d90a629ef
Amit Sabal <amit.sabal@impelsys.com>
2019-03-08 18:31:55 +0530

    Code to extract URL for the link ‘Submit Paper’ completed

--- app.php
+++ app.php
@@ -10,46 +10,38 @@
 if(php_sapi_name() === 'cli'){
     
 	//URL of the page to be parsed
-	$url = "https://journals.sagepub.com/home/VRT";
+    $url = "sourceFile.html";
     
-   /*
     //Initializing DOM object
     libxml_use_internal_errors(true);
     $doc = new DOMDocument();
 	
 	//Reading the content of the url
     $doc->loadHTMLFile($url);
-	
-	//iterating loop through entire page content by using html tag 'a'
-    foreach( $doc->getElementsByTagName('a') as $item){
-		
-		//Fetching the value of the anchor tag , by using the its attribute 'href'
-        $href =  $item->getAttribute('href');
-        var_dump($href);
-    }
-    */
-   
 
-    /**
-     * Getting web page content using cURL
-     */
-    $c = curl_init($url);
-    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($c, CURLOPT_FOLLOWLOCATION, TRUE);
-   
-    $html = curl_exec($c);
-    //echo $html;
-    $outfile = "url2.txt";
-    file_put_contents($outfile, $html);
-    
-    if (curl_error($c))
-        die(curl_error($c));
+    //Saving the page content to dom object
+    $doc->saveHTML();
 
-    // Get the status code
-    $status = curl_getinfo($c, CURLINFO_HTTP_CODE);
-    
-    curl_close($c);
+    //Looping the content by tag name 'li'
+    foreach( $doc->getElementsByTagName('li') as $item){
+        
+        //Fetching the node value name
+        $value =$item->nodeValue;
+       
+        //If condition chech for the nodeValue to be Submit Paper
+        if (trim($value) === "Submit Paper") { 
+
+            //iterating loop through entire page content by using html tag 'a'
+            foreach( $item->getElementsByTagName('a') as $link){
+
+                //Fetching the value of the anchor tag , by using the its attribute 'href'
+                $href =  $link->getAttribute('href');
+
+                echo "\n Key => ".$value."  Link => ".$href;               
+            }
+        }
+    }    
+}else{
+    echo "Access Denied";
 }
 ?>
